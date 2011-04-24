9c4830aa572a687e4dd45e919bbd199a9fa39550
mtaylor <mtaylor@f3314826-8319-4489-91bc-5fb6ec4ec210>
2011-04-24 07:57:20 +0000

    Created MjtWebGlCube.  Lots of optimizations.
    git-svn-id: file:///Users/mtaylor/Documents/svn/CanvasWorld@6 f3314826-8319-4489-91bc-5fb6ec4ec210

--- trunk/WebContent/js/main.js
+++ trunk/WebContent/js/main.js
@@ -25,52 +25,17 @@ function MjtWebGlToolkit()
 		}
 	};
 
-	this.vertexShaderAttributeInitialized = {};
 	
-	this.addVertexShaderAttribute = function addVertexShaderAttribute(attributeIdx, bufferObject, componentPrimitiveType, vertexDimensions)
+	this.addVertexShaderAttribute = function addVertexShaderAttribute(attributeName, bufferObject, componentPrimitiveType, vertexDimensions)
 	{
+		var attributeIdx = this.gl.getAttribLocation(this.gl.program, attributeName);
 		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, bufferObject);
-		if(!this.vertexShaderAttributeInitialized[attributeIdx])
-		{
-			this.gl.vertexAttribPointer(attributeIdx, vertexDimensions, componentPrimitiveType, false, 0, 0);
-			this.vertexShaderAttributeInitialized[attributeIdx] = true;
-		}	
+		this.gl.vertexAttribPointer(attributeIdx, vertexDimensions, componentPrimitiveType, false, 0, 0);
 		//this.gl.enableVertexAttribArray(attributeIdx);
-		//this.gl.vertexAttribPointer(attributeIdx, vertexDimensions, componentPrimitiveType, false, 0, 0);
 	};
 
 	this.protoCube = null;
 
-	this.createCube = function createCube(positionArray, scale, color)
-	{
-
-		if (this.protoCube == null)
-		{
-			this.protoCube = makeBox(this.gl);
-
-			// Set up the array of colors for the cube's faces
-			this.protoCube.colors = new Uint8Array([ 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, // v0-v1-v2-v3 front
-			1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, // v0-v3-v4-v5 right
-			0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, // v0-v5-v6-v1 top
-			1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, // v1-v6-v7-v2 left
-			1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, // v7-v4-v3-v2 bottom
-			0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1 ] // v4-v7-v6-v5 back
-			);
-
-			// Set up the vertex buffer for the colors
-			this.protoCube.colorObject = this.gl.createBuffer();
-
-			this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.protoCube.colorObject);
-			this.gl.bufferData(this.gl.ARRAY_BUFFER, this.protoCube.colors, this.gl.STATIC_DRAW);
-
-		}
-
-		var result = {};
-
-		result.positionArray = positionArray;
-
-		return result;
-	};
 
 	this.init = function init()
 	{
@@ -86,52 +51,34 @@ function MjtWebGlToolkit()
 		// used later.
 		this.VERTEX_ATTRIBUTES,
 		// The clear color and depth values
-		[ 0, 0, 0, 1 ], 10000);
+		[ 0, 0, 0, 0.1], 10000);
 		if (!this.gl)
 		{
 			throw "Unable to initialize WebGL";
 		}
 
+		this.protoCube = makeBox(this.gl);
+		
 		// Set up light direction uniform
 		this.gl.uniform3f(this.gl.getUniformLocation(this.gl.program, "lightDir"), 0, 0, 1);
 
 		this.u_modelViewMatrixLoc = this.gl.getUniformLocation(this.gl.program, "u_modelViewMatrix");
 		this.u_projMatrixLoc = this.gl.getUniformLocation(this.gl.program, "u_projMatrix");
 		this.u_normalMatrixLoc = this.gl.getUniformLocation(this.gl.program, "u_normalMatrix");
+		this.u_cameraTranslationMatrixLoc = this.gl.getUniformLocation(this.gl.program, "u_cameraTranslationMatrix");
+
+		
+		
 		this.gl.enableVertexAttribArray(0);
 		this.gl.enableVertexAttribArray(1);
 		this.gl.enableVertexAttribArray(2);
+		this.addVertexShaderAttribute("vNormal", this.protoCube.normalObject, this.gl.FLOAT, 3);
+		this.addVertexShaderAttribute("vPosition", this.protoCube.vertexObject, this.gl.FLOAT, 3);
+		this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, mjtWebGlToolkit.protoCube.indexObject);
 		
 		this.log("Finished init.");
 	};
 
-	this.drawGeometricObject = function drawCube(perspectiveMatrix, modelViewMatrix, geometricObject)
-	{
-
-		// this.log(geometricObject.mvMatrix.toString());
-		this.setUniforms(perspectiveMatrix, modelViewMatrix, geometricObject.positionArray);
-		// Draw the geometric object
-		this.gl.drawElements(this.gl.TRIANGLES, this.protoCube.numIndices, this.gl.UNSIGNED_BYTE, 0);
-
-	};
-
-	this.modelViewMatrix = new J3DIMatrix4();
-	this.normalMatrix = new J3DIMatrix4();
-	this.setUniforms = function setUniforms(perspectiveMatrix, modelViewMatrix, positionArray)
-	{
-
-		modelViewMatrix.translate(positionArray);
-		//modelViewMatrix.rotate(this.currentAngle,0,1,0);
-		modelViewMatrix.setUniform(this.gl, this.u_modelViewMatrixLoc, false);
-
-		// Construct the normal matrix from the model-view matrix and pass it in
-		// normal is only used for determining color
-		var normalMatrix = this.normalMatrix;
-		normalMatrix.load(modelViewMatrix);
-		normalMatrix.invert();
-		normalMatrix.transpose();
-		normalMatrix.setUniform(this.gl, this.u_normalMatrixLoc, false);
-	};
 
 	this._perspectiveMatrix = null;
 
@@ -152,10 +99,6 @@ function MjtWebGlToolkit()
 		var canvas = document.getElementById(this.canvasId);
 		var windowWidth = window.innerWidth - 80;
 		var windowHeight = window.innerHeight - 80;
-		if (windowWidth == this.width && windowHeight == this.height && this._perspectiveMatrix != null)
-		{
-			return this._perspectiveMatrix;
-		}
 
 		this.width = windowWidth;
 		this.height = windowHeight;
@@ -165,14 +108,11 @@ function MjtWebGlToolkit()
 		// Set the viewport and projection matrix for the scene
 		this.gl.viewport(0, 0, this.width, this.height);
 		var perspectiveMatrix = new J3DIMatrix4();
+//		this.perspectiveMatrix.makeIdentity();
+//		var perspectiveMatrix = this.perspectiveMatrix;
 
 		// fovy (field of view? y), aspect, zNear, zFar
-		perspectiveMatrix.perspective(45, this.width / this.height, 0.1, 100);
-
-		// eyex, eyey, eyez, centerx, centery, centerz, upx, upy, upz
-		// this.log("eye: " + this.camera.eye)
-		// this.log("center: " + this.camera.center)
-		// perspectiveMatrix.lookat(this.camera.eye.getAsArray(), this.camera.center.getAsArray(), this.camera.up.getAsArray());
+		perspectiveMatrix.perspective(45, this.width / this.height, 0.1, 1000);
 
 		this._perspectiveMatrix = perspectiveMatrix;
 		return perspectiveMatrix;
@@ -183,38 +123,84 @@ function MjtWebGlToolkit()
 
 	this.geometricObjects = [];
 
+	this.modelViewMatrixOldValues = {};
+	
+	this.isOldValueDifferent = function testOldValue(valueName, value)
+	{
+		var oldValue = this.modelViewMatrixOldValues[valueName];
+		var result = oldValue != value;
+		this.modelViewMatrixOldValues[valueName] = value;
+		return result;
+	};
+	
+	this.isModelViewMatrixUpdateRequired = function isModelViewMatrixUpdateRequired()
+	{
+		
+		var result = false;
+		
+		if(this.isOldValueDifferent("pitch", pitch))
+		{
+			result = true;
+		}	
+		if(this.isOldValueDifferent("yaw", yaw))
+		{
+			result = true;
+		}	
+		if(this.isOldValueDifferent("posx", posx))
+		{
+			result = true;
+		}	
+		if(this.isOldValueDifferent("posy", posy))
+		{
+			result = true;
+		}	
+		if(this.isOldValueDifferent("posz", posz))
+		{
+			result = true;
+		}	
+		return result;
+	};
+
+	this.normalMatrix = new J3DIMatrix4();
+	this.modelViewMatrix = new J3DIMatrix4();
+	this.cameraTranslationMatrix = new J3DIMatrix4();
+
 	this.drawPicture = function drawPicture()
 	{
-
-		// Make sure the canvas is sized correctly.
-		var perspectiveMatrix = this.reshape(this.gl);
-		perspectiveMatrix.setUniform(this.gl, this.u_projMatrixLoc, false);
-
 		// Clear the canvas
 		this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
 
-		var modelViewMatrix = this.modelViewMatrix;
-		modelViewMatrix.makeIdentity();
-		modelViewMatrix.rotate(-pitch, 1, 0, 0);
-		modelViewMatrix.rotate(-yaw, 0, 1, 0);
-		modelViewMatrix.translate(-posx, -posy, -posz);
+		//var cameraTranslationMatrix = new J3DIMatrix4();
+		this.cameraTranslationMatrix.makeIdentity();
+		this.cameraTranslationMatrix.rotate(-pitch, 1, 0, 0);
+		this.cameraTranslationMatrix.rotate(-yaw, 0, 1, 0);
+		this.cameraTranslationMatrix.translate(-posx, -posy, -posz);
+		this.cameraTranslationMatrix.setUniform(this.gl, this.u_cameraTranslationMatrixLoc, false);
+		
+		// Make sure the canvas is sized correctly.
+		var perspectiveMatrix = this.reshape(this.gl);
+		//perspectiveMatrix.multiply(this.cameraTranslationMatrix);
+		perspectiveMatrix.setUniform(this.gl, this.u_projMatrixLoc, false);
 
-		var mvMatrix = new J3DIMatrix4();
 
-		for ( var i in this.geometricObjects)
+//		var modelViewMatrix = this.modelViewMatrix;
+//
+//		var mvMatrix = new J3DIMatrix4();
+		
+		var normalMatrix = this.normalMatrix;
+		normalMatrix.load(this.cameraTranslationMatrix);
+		normalMatrix.invert();
+		normalMatrix.transpose();
+		normalMatrix.setUniform(this.gl, mjtWebGlToolkit.u_normalMatrixLoc, false);
+
+		
+		for (var i in this.geometricObjects)
 		{
-			// this.log("drawing object: " + i + "at angle: " + this.currentAngle);
 			var geometricObject = this.geometricObjects[i];
-			// geometricObject.mvMatrix.rotate(this.currentAngle, 0, 1, 0);
-			mvMatrix.load(modelViewMatrix);
-			this.addVertexShaderAttribute(0, this.protoCube.normalObject, this.gl.FLOAT, 3);
-			this.addVertexShaderAttribute(1, this.protoCube.colorObject, this.gl.UNSIGNED_BYTE, 4);
-			this.addVertexShaderAttribute(2, this.protoCube.vertexObject, this.gl.FLOAT, 3);
-			// Bind the index array
-			this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.protoCube.indexObject);
-
-			this.drawGeometricObject(perspectiveMatrix, mvMatrix, geometricObject);
+			geometricObject.paint(this.gl);
 		}
+		
+		this.gl.flush();
 
 		// Show the framerate
 		//console.log("drawing Picture:")
@@ -241,46 +227,22 @@ function MjtWebGlToolkit()
 		this.init();
 		this.framerate = new Framerate("framerate");
 
-		var x = 50;
-		var y = 5;
+		var x = 20;
+		var y = 20;
 		
 		for (var j = 0; j <= y; j++)
 		{	
 			for ( var i = -x; i <= x; i++)
 			{
-				var cube = this.createCube([ i * 2.1, j * 2.1, 0 ]);
+				//var cube = this.createCube([ i * 2.1, j * 2.1, 0 ]);
+				var cube = new MjtWebGlCube(this.gl, [ i * 2.1, j * 2.1, 0 ], 1 + (0.1* i), [10*i,20*i,30*i]);
+				//cube.positionArray = [ i * 2.1, j * 2.1, 0 ];
 				this.geometricObjects.push(cube);
 	
 			}
 		}
 		 
 
-		// {
-		// var cube = this.createCube([-4,0,0]);
-		// this.geometricObjects.push(cube);
-		// }
-		// {
-		// var cube = this.createCube([4,0,0]);
-		// this.geometricObjects.push(cube);
-		// }
-//		{
-//			var cube = this.createCube([ 0, 0, 0 ]);
-//			// cube.mvMatrix.scale(0.2,0.2,0.2);
-//			this.geometricObjects.push(cube);
-//		}
-		{
-			// var cube = this.createCube();
-			// cube.mvMatrix.scale(20,20,20);
-			// this.geometricObjects.push(cube);
-		}
-
-		{
-			// var cube = this.createCube();
-			// cube.mvMatrix.scale(20,20,20);
-			// this.geometricObjects.push(cube);
-		}
-
-		// this.drawPicture();
 
 		tk = this;
 		tk.lastTime = new Date().getTime();
---
+++ trunk/WebContent/js/mjt-webgl-cube.js
@@ -0,0 +1,53 @@
+
+
+function MjtWebGlCube(context, positionArray, scale, rotationArray)
+{
+	this.context = context;
+	this.positionArray = positionArray;
+	this.rotationArray = rotationArray;
+	this.scale = scale;
+	
+	this.init = function init()
+	{
+		// Set up the array of colors for the cube's faces
+		this.colors = new Uint8Array([ 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, // v0-v1-v2-v3 front
+		1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, // v0-v3-v4-v5 right
+		0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, // v0-v5-v6-v1 top
+		1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, // v1-v6-v7-v2 left
+		1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, // v7-v4-v3-v2 bottom
+		0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1 ] // v4-v7-v6-v5 back
+		);
+
+		// Set up the vertex buffer for the colors
+		this.colorObject = context.createBuffer();
+
+		this.context.bindBuffer(context.ARRAY_BUFFER, this.colorObject);
+		this.context.bufferData(context.ARRAY_BUFFER, this.colors, context.STATIC_DRAW);
+		
+		mjtWebGlToolkit.addVertexShaderAttribute("vColor", this.colorObject, context.UNSIGNED_BYTE, 4);
+
+		//mjtWebGlToolkit.addVertexShaderAttribute("vPosition", mjtWebGlToolkit.protoCube.vertexObject, this.context.FLOAT, 3);
+		
+		// Bind the index array
+		//this.context.bindBuffer(this.context.ELEMENT_ARRAY_BUFFER, mjtWebGlToolkit.protoCube.indexObject);
+
+		this.modelViewMatrix = new J3DIMatrix4();
+		this.modelViewMatrix.translate(this.positionArray);
+//		this.modelViewMatrix.scale(this.scale);
+//		this.modelViewMatrix.rotate(this.rotationArray[0],1,0,0);
+//		this.modelViewMatrix.rotate(this.rotationArray[1],0,1,0);
+//		this.modelViewMatrix.rotate(this.rotationArray[2],0,0,1);
+		//modelViewMatrix.setUniform(this.context, mjtWebGlToolkit.u_modelViewMatrixLoc, false);
+
+	};
+	
+	
+	this.paint = function paint(context)
+	{
+		this.modelViewMatrix.setUniform(this.context, mjtWebGlToolkit.u_modelViewMatrixLoc, false);
+		context.drawElements(context.TRIANGLES, mjtWebGlToolkit.protoCube.numIndices, context.UNSIGNED_BYTE, 0);
+	};
+
+	
+	this.init();
+}
