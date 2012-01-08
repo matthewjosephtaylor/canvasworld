f1772c1a4f0727d1b2e1753c2d4fb6107ec4884f
Matt Taylor <matt@matthewjosephtaylor.com>
2012-01-08 11:39:28 -0600

    WIP

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -1,22 +1,13 @@
-//mjt.require("MjtWebGlScene", "MjtWebGlToolkit", "MjtLogger", function mainCallback()
-mjt.require("MjtLogger", function mainCallback() {
+//mjt.require("MjtLogger", function mainCallback() {
+
+mjt.require("MjtWebGlScene", "MjtWebGlToolkit", "MjtLogger", function mainCallback() {
 
 	console.log("start main...");
-	// MjtWebGlToolkit.getInstance();
-	// mjtWebGlScene
-	// =
-	// MjtWebGlScene.getInstance();
-	// mjtWebGlScene.createNew();
+	MjtWebGlToolkit.getInstance();
+	mjtWebGlScene = MjtWebGlScene.getInstance();
+	mjtWebGlScene.createNew();
 
 	console.log("end main.");
-
-	var f = function(foo) {
-		var logger = new MjtLogger();
-		//logger.log("hello");
-		logger.trace();
-	};
-	
-	f("bar");
 
 }
 
